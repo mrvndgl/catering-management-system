f50482d740b53e0494dffc92e48840ef98ec7b95
mrvndgl <johnmarvindigal@gmail.com>
2025-03-30 11:32:36 +0800

    Styles

--- admin-staff-frontend/vite.config.js
+++ admin-staff-frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["html2pdf.js"],
+  },
   server: {
     port: 5174,
     proxy: {
